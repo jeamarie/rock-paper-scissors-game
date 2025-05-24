c94d851640041d2fd74eb1c9d1aff2cbfa313f45
jeamarie <enadjeamarie@gmail.com>
2025-05-25 02:58:55 +0800

    Revise the strings content

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let computerScore = 0;
 let humanScore = 0;
 
-// function for randomizing the computer's choice of weapon
+// function for randomizing the computer's choice
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice == 0) {
@@ -14,9 +14,9 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-// function for getting the human's choice of weapon
+// function for getting the human's input
 function getHumanChoice() {
-    let humanChoice = prompt("Choose your weapon:").toLowerCase();
+    let humanChoice = prompt("Choose your weapon (rock, paper, scissors):").toLowerCase();
     return humanChoice;
 }
 
@@ -24,45 +24,45 @@ function getHumanChoice() {
 function playGame() {
 
     console.log(playRound(getHumanChoice(), getComputerChoice()));
-    if ((humanScore < 5) && (computerScore < 5)){
+    if ((humanScore < 5) && (computerScore < 5)) {
         playGame();
     } else {
         console.log(endGame());
     }
 
-// function for one round
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-        
-        return `Scores = ${humanScore}, ${computerScore}
-            It's a draw...`;
-    } else if ((humanChoice == "rock" && computerChoice == "scissors")
-        || (humanChoice == "scissors" && computerChoice == "paper")
-        || (humanChoice == "paper" && computerChoice == "rock")) {
-        humanScore++;
-        return `Scores = ${humanScore}, ${computerScore}
-            Nice! Your ${humanChoice} beats the computer's ${computerChoice}.`;
-    } else {
-        computerScore++;
-        return `Scores = ${humanScore}, ${computerScore}
-            Uh oh! The computer's ${computerChoice} beats your ${humanChoice}.`;
-    }
-}
+    // function for one round
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
 
-// function that ends the game
+            return `Your score = ${humanScore}, Opponent's score = ${computerScore}
+            It's a draw...`;
+        } else if ((humanChoice == "rock" && computerChoice == "scissors")
+            || (humanChoice == "scissors" && computerChoice == "paper")
+            || (humanChoice == "paper" && computerChoice == "rock")) {
+            humanScore++;
+            return `Your score = ${humanScore}, Opponent's score = ${computerScore}
+            Nice! Your ${humanChoice} beat(s) the opponents's ${computerChoice}.`;
+        } else {
+            computerScore++;
+            return `Your score = ${humanScore}, Opponent's score = ${computerScore}
+            Uh oh! The opponent's ${computerChoice} beat(s) your ${humanChoice}.`;
+        }
+    }
+
+    // function that ends the game
     function endGame() {
         if (humanScore > computerScore) {
             return `You win!
             Your score = ${humanScore}
-            Computer's score = ${computerScore}`
+            Opponent's score = ${computerScore}`
         } else if (humanScore < computerScore) {
             return `You lose!
             Your score = ${humanScore}
-            Computer's score = ${computerScore}`
+            Opponent's score = ${computerScore}`
         } else {
             return `It's a draw.
             Your score = ${humanScore}
-            Computer's score = ${computerScore}`
+            Opponent's score = ${computerScore}`
         };
     }
 
