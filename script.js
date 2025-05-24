0f1ddc481a41a11a0498a5376b6f82ba39fee7e8
jeamarie <enadjeamarie@gmail.com>
2025-05-24 18:32:54 +0800

    Create the playRound function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-console.log("Hello, World!");
+// global variables
+let computerScore = 0;
+let humanScore = 0;
 
+// function for randomizing the computer's choice of weapon
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice == 0) {
@@ -12,12 +15,32 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
+// function for getting the human's choice of weapon
 function getHumanChoice() {
-    let humanChoice = prompt("Choose your weapon:");
+    let humanChoice = prompt("choose your weapon:");
     return humanChoice;
 }
 
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
 
+// function for a round of play
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice == "rock" && computerChoice == "rock")
+        || (humanChoice == "scissors" && computerChoice == "scissors")
+        || (humanChoice == "paper" && computerChoice == "paper")) {
+        return "it's a tie...";
+    } else if ((humanChoice == "rock" && computerChoice == "scissors")
+        || (humanChoice == "scissors" && computerChoice == "paper")
+        || (humanChoice == "paper" && computerChoice == "rock")) {
+        humanScore++;
+        return `you win! ${humanChoice} beats ${computerChoice}.`;
+    } else {
+        computerScore++;
+        return `you lose! ${computerChoice} beats ${humanChoice}.`;
+    }
+}
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
+console.log(`human = ${humanScore}, computer = ${computerScore}`);
